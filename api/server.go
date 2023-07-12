b256a3c1435a9abb795aeb0e2f2b4dd9231edbcf
M. Maahir Maulana <hidarizero4869@gmail.com>
2023-07-13 00:12:09 +0700

    (+) Login token : new server

--- api/server.go
+++ api/server.go
@@ -1,20 +1,32 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/go-playground/validator/v10"
 	db "github.com/maulana48/backend_master_class/simplebank/db/sqlc"
+	"github.com/maulana48/backend_master_class/simplebank/token"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 )
 
 type Server struct {
-	Store  db.Store
-	Router *gin.Engine
+	Store      db.Store
+	TokenMaker token.Maker
+	Router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{Store: store}
+func NewServer(store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker("")
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker : %w", err)
+	}
+
+	server := &Server{
+		Store:      store,
+		TokenMaker: tokenMaker,
+	}
 	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
@@ -29,7 +41,7 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfers", server.createTransfer)
 
 	server.Router = router
-	return server
+	return server, nil
 }
 
 func (server *Server) Start(address string) error {
